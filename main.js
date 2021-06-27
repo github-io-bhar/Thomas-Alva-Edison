e70e410f372f0d6603f74522c07bc0eef4f31936
github-io-bhar <86400328+github-io-bhar@users.noreply.github.com>
2021-06-27 13:50:43 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function TurnOn() {
+document.getElementById("bulb").src = "https://s3-projectsassets.whjr.online/On+Bulb.png";
+}
+function TurnOff() {
+document.getElementById("bulb").src = "https://s3-projectsassets.whjr.online/Off+Bulb.png";
+}
